baa64ddc8e620ebfe9e92bf6dd831153ae963113
Luiz Otávio Lima Falcão <luizotavio-falcao@hotmail.com>
2021-10-15 23:09:02 -0300

    Fix form not submitting when using the keyboard

--- src/pages/Home/styled.jsx
+++ src/pages/Home/styled.jsx
@@ -1,61 +1,61 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const segoeUI = "'Segoe UI', sans-serif";
 
 export const HomeContainer = styled.div`
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const Content = styled.div`
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+export const Content = styled.form`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Input = styled.input`
-    width: 20rem;
-    border: 1px solid #78aeff;
-    border-right: transparent;
-    height: 3rem;
-    padding: 0 0.5rem;
-    border-radius: 0.25rem 0 0 0.25rem;
-    font-family: ${segoeUI};
+  width: 20rem;
+  border: 1px solid #78aeff;
+  border-right: transparent;
+  height: 3rem;
+  padding: 0 0.5rem;
+  border-radius: 0.25rem 0 0 0.25rem;
+  font-family: ${segoeUI};
 
-    &:focus,
-    &:active {
-        outline: none;
-        box-shadow: none;
-    }
+  &:focus,
+  &:active {
+    outline: none;
+    box-shadow: none;
+  }
 `;
 
 export const Button = styled.button`
-    height: 100%;
-    padding: 0 2rem;
-    background: #092250;
-    color: #eee;
-    border: 1px solid #78aeff;
-    border-left: transparent;
-    border-radius: 0 0.25rem 0.25rem 0;
-    cursor: pointer;
-    font-family: ${segoeUI};
-    font-size: 1rem;
+  height: 100%;
+  padding: 0 2rem;
+  background: #092250;
+  color: #eee;
+  border: 1px solid #78aeff;
+  border-left: transparent;
+  border-radius: 0 0.25rem 0.25rem 0;
+  cursor: pointer;
+  font-family: ${segoeUI};
+  font-size: 1rem;
 
-    &:focus,
-    &:active {
-        outline: none;
-        box-shadow: none;
-    }
+  &:focus,
+  &:active {
+    outline: none;
+    box-shadow: none;
+  }
 `;
 
 export const ErrorMsg = styled.span`
-    display: block;
-    font-size: 0.9rem;
-    color: #dadada;
-    font-weight: 600;
-    margin: 1rem;
+  display: block;
+  font-size: 0.9rem;
+  color: #dadada;
+  font-weight: 600;
+  margin: 1rem;
 `;
